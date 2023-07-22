b14f67ea6a4f7e2f7b543d161c06027343b53b5c
Simon Mravec <simon.mravec@gmail.com>
2023-07-22 10:02:10 +0200

    Visual fixes, menu

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,18 @@
 <script >
   import gridEval from "./gridEval.js"
-  import { onMount } from "svelte"
 
-  gridEval()
+  //gridEval()
   let grid = [["","",""],["","",""],["","",""]];
   let turn = "X"
   let won = false
+  let score_ai = 0
+  let score_human = 0
 
   function gridUpdate(i1, i2) {
     if( grid[i1][i2] === ""){
       //place x or o at selected spot
       grid[i1][i2] = turn
-
+      
       //change the turn
       if(turn === "X"){
 	turn = "O"
@@ -21,19 +22,30 @@
       }
     }
   }
-
-  onMount(() => {
-    console.log("this is on mount")
-    const canvas = document.querySelector('canvas')
-    const context = canvas.getContext('2d');
-  })
-
 </script>
 
 <main>
   
-  <canvas>
-  </canvas>
+  <div class="menu"> 
+    Score {score_human}:{score_ai}
+    <button class="restart-btn" 
+	    on:click={()=>{grid = [["","",""],["","",""],["","",""]]; won=false}}>
+    {won == false ? "reset" : "restart"}
+    </button>
+  </div>
+
+  <div class="gridContainer">
+    {#each grid as row ,index1}
+    <div>
+    {#each row as item,index2}  
+      <button on:click={() => { if(won === false){gridUpdate(index1,index2)}}} 
+	      class={grid[index1][index2] === "O" || grid[index1][index2] === "X" ? "box" : "box-empty"}>
+	{item}
+      </button> 
+    {/each}
+    </div>
+    {/each}
+  </div>
 
 </main>
 
@@ -44,11 +56,75 @@
     justify-content: center;
     align-items: center;
     height: 100vh;
+    overflow:hidden;
   }
 
-  canvas{
-    border: white solid 1px;
-    width: 50vw;
-    height: 50vw;
-  } 
+  .menu{
+    color: white;
+    position: absolute;
+    top:9vh;
+    display:flex;
+    flex-direction:column;
+    font-size:30px;
+  }
+
+  .restart-btn{
+    font-size:20px;
+    margin-top:20px;
+    background:transparent;
+    border:1px solid gray;
+    border-radius:5px;
+    padding: 5px;
+    color:white;
+    cursor:pointer;
+  }
+  
+  .gridContainer{
+    width:50vw;
+    height:50vw;
+    position:absolute;
+  }
+
+  .box-empty, .box{
+    width: calc(50vw/3);
+    height: calc(50vw/3);
+    background-color: transparent;
+    border: 1px solid #99999988;
+    color: white;
+    font-size: 30px;
+    vertical-align: middle;
+  }
+
+  .box-empty:hover{
+    background-color: #00000055;;
+  }
+     
+  .box:active {
+    animation: notEmpty 0.1s forwards;
+  }
+
+  @keyframes notEmpty{
+    100%{
+      background-color: #990000;
+    }
+  }
+
+  @media only screen and (min-aspect-ratio: 1/1){
+    .menu{
+      flex-direction: row;
+      top: 7vh;
+      font-size:30px;
+    }
+
+    .gridContainer{
+      width:50vh;
+      height:50vh;
+    }
+
+    .box-empty, .box{
+      width: calc(50vh/3);
+      height: calc(50vh/3);
+    }
+  }
+
 </style>
